8db7d23400e1ef4465590b0fea01cdc2bcb5fffc
YuDeokRin <ejrfls12@naver.com>
2022-01-10 23:45:40 +0900

    View 분리 - v2

---
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberFormControllerV2.java
@@ -0,0 +1,21 @@
+package hello.servlet.web.frontcontroller.v2.controller;
+
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberFormControllerV2 implements ControllerV2 {
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+//        jsp로 보내는 dispatcher 부분이  아래와 같이 간략하게 된다.
+//        MyView myView = new MyView("/WEB-INF/views/new-form.jsp");
+//        return myView;
+        return new MyView("/WEB-INF/views/new-form.jsp");
+    }
+}
